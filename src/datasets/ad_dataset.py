24d720b404f883cbd0e93fc74b925f04809629a0
Bryan Chan <bryanchan13@gmail.com>
2025-06-04 13:05:28 -0600

    Add bandit dataset for AD

--- src/datasets/ad_dataset.py
+++ src/datasets/ad_dataset.py
@@ -1,3 +1,4 @@
+import _pickle as pickle
 import h5py
 import numpy as np
 
@@ -73,6 +74,67 @@ class XMiniGridADDataset(IterableDataset):
                 start_idx : start_idx + self.seq_len
             ]
             rewards = self.data_file[task_id]["rewards"][learning_history_idx][
+                start_idx : start_idx + self.seq_len
+            ]
+
+            yield {
+                "state": states, # (seq_len, 5, 5, 2)
+                "action": actions, # (seq_len,)
+                "reward": rewards, # (seq_len,)
+                "target": actions, # (seq_len,)
+            }
+
+
+class BanditADDataset(IterableDataset):
+    """
+    Data is collected using rejax.
+    """
+
+    def __init__(
+        self,
+        data_path: str,
+        seq_len: int,
+        seed: int,
+    ):
+        self.seq_len = seq_len
+        self.data_path = data_path
+        self.seed = seed
+        self._rng = np.random.RandomState(seed)
+
+        with pickle.load(open(data_path, "rb")) as f:
+            self.env_params = f["env_params"]
+            self.buffer = f["buffer"]["data"]
+            self.task_ids = np.arange(len(self.env_params))
+            self.num_arms = self.env_params.shape[-1]
+            self.num_tasks = len(self.task_ids)
+            self.hists_per_task = 1
+
+            # Exclude very last sample per history
+            self.max_len = self.buffer.reward.shape[-1] - seq_len - 1
+
+    @property
+    def observation_space(self):
+        return spaces.Box(low=0, high=1, shape=(1,), dtype=int)
+
+    @property
+    def action_space(self):
+        return spaces.Discrete(self.num_arms)
+
+    def __iter__(self):
+        return iter(self.get_sequences())
+
+    def get_sequences(self):
+        while True:
+            task_id = self._rng.choice(self.task_ids)
+            start_idx = self._rng.randint(self.max_len)
+
+            states = self.buffer["obs"][task_id][
+                start_idx : start_idx + self.seq_len
+            ]
+            actions = self.buffer["actions"][task_id][
+                start_idx : start_idx + self.seq_len
+            ]
+            rewards = self.buffer["rewards"][task_id][
                 start_idx : start_idx + self.seq_len
             ]
 
