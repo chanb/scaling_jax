7d1295e937cb8d77f7e900995462e2ef1c6e4b74
Bryan Chan <bryan.chan@ualberta.ca>
2025-11-18 08:01:40 -0700

    Update Metastable PPO

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -6,8 +6,9 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from src.learners.reinforce import REINFORCE
+from src.learners.metastable_ppo import MetastablePPO
 from src.learners.metastable_reinforce import MetastableREINFORCE
 from src.learners.ppo import PPO
+from src.learners.reinforce import REINFORCE
 from src.learners.supervised import Supervised
 
