f3a9700debd319ff7029211dfefde4a2f499bca5
Bryan Chan <bryan.chan@ualberta.ca>
2026-01-23 12:10:32 -0700

    Remove unused for now

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -6,7 +6,6 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from src.learners.metastable_ppo import MetastablePPO
 from src.learners.ppo import PPO
 from src.learners.reinforce import REINFORCE
 from src.learners.supervised import Supervised
